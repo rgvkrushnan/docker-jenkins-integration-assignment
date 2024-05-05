2b6f1fef12b6438e479f560c09489dd30cfaeb46
Guru <guru09295@gmail.com>
2024-05-05 19:39:11 +0530

    trial 3

--- src/main/java/com/techie/Main.java
+++ src/main/java/com/techie/Main.java
@@ -5,7 +5,7 @@ package com.techie;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello 123 and welcome 123 !");
 
     }
 }
