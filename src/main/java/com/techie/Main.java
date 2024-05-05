38e2e3a83498a2b8ff47a8674a1f58b8ae5fedac
Guru <guru09295@gmail.com>
2024-05-05 21:37:37 +0530

    changes to java

--- src/main/java/com/techie/Main.java
+++ src/main/java/com/techie/Main.java
@@ -1,11 +1,9 @@
 package com.techie;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello 123 and welcome 123 !");
+        System.out.print("Hello 123 and welcome 123!");
 
     }
 }
